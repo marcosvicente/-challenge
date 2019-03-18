f2a06a1d83a5df14c8b7421b4adeccb55bc95595
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2019-03-18 13:10:54 -0300

    add config factory

--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -1,14 +1,3 @@
 RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-
+    config.include FactoryBot::Syntax::Methods
 end
